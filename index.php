f0216a5ce8b99ce796d042955009f1c394232079
fabien <flierville@cesi.fr>
2023-12-12 14:47:39 +0100

    $_GET

--- index.php
+++ index.php
@@ -3,7 +3,11 @@ require("./inc/config.php");
 require("./inc/header.php");
 ?>
 <h1>Bienvenue sur notre BLOG</h1>
-<!-- Tout le HTML de la page d'accueil -->
+<!-- Je souhaite
+- Sur index.php une liste html de 5 lien (balise <a>) élèves (Enzo ...)
+- Pour chaque élève un lien vers eleve.php (à créer)
+- eleve.php afiche Bonjour {eleve} ta note est de {note}
+-->
 
 
 
