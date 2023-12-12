e9b79203ae53cdb7fc9a1bdbc5ad2ac294ac64d1
fabien <flierville@cesi.fr>
2023-12-12 15:35:10 +0100

    push pour loup

---
+++ admin/article_add_form.php
@@ -0,0 +1,13 @@
+<?php
+/*
+ * dans article_add_form.php faire un formulaire pour « Ajouter un article » dont voici les champs nécessaires :
+-	Titre : input type text
+-	Description : texarea
+-	Date de publication : input type date
+-	Auteur : un <select> avec 5 <option> que sont les prénoms de camarades de classes
+Attention, pensez à bien mettre un attribut « name » à vos éléments de formulaire
+Créer /admin/article_show.php : récupérez les données du formulaire et afficher l’article dans du HTML.
+
+Dans les 2 fichiers Pensez aux require pour intégrer le footer et header html.
+
+ */
--- index.php
+++ index.php
@@ -9,6 +9,17 @@ require("./inc/header.php");
     <li><a href="eleve.php?nom=Evan&note=A">Evan</a></li>
     <li><a href="eleve.php?nom=Raphael&note=B">Raphael</a></li>
 </ul>
+<form method="post" action="submitform.php">
+    <input type="text" placeholder="Rechercher ..." name="search">
+    <input type="hidden" value="1234" name="id">
+</form>
+
+    <form method="post">
+        <input type="text" name="prenom">
+        <input type="text" name="nom">
+        <input type="date" name="naissance">
+        <input type="submit">
+    </form>
 
 
 <?php   require("./inc/footer.php"); ?>
