9c8da6f547fd495690de58a148267801a1793152
fabien <flierville@cesi.fr>
2023-12-13 10:08:26 +0100

    article add

--- admin/article_add_script.php
+++ admin/article_add_script.php
@@ -4,8 +4,10 @@ require ("../inc/config.php");
 $requete = $bdd->prepare("INSERT INTO articles (Titre, Description, DatePublication, Auteur) VALUES(:Titre, :Description,:DatePublication, :Auteur)");
 
 $requete->execute([
-    "Titre" => "Mon troisième article",
-    "Description" => "Ceci est mon troisième article",
-    "DatePublication" => "2023-12-13",
-    "Auteur" => "Rémi",
+    "Titre" => $_POST["Titre"],
+    "Description" => $_POST["Description"],
+    "DatePublication" => $_POST["DatePublication"],
+    "Auteur" => $_POST["Auteur"],
 ]);
+
+header("Location:/admin");
