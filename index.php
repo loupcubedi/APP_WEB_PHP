50444fac5964753e34f54d52f900077be07b7389
fabien <flierville@cesi.fr>
2023-12-13 09:23:47 +0100

    BDD

--- index.php
+++ index.php
@@ -1,25 +1,21 @@
 <?php
 require("./inc/config.php");
 require("./inc/header.php");
+if($_POST){
+    $requete = $bdd->query("SELECT * FROM articles WHERE Id={$_POST["search"]}");
+}else{
+    $requete = $bdd->query("SELECT * FROM articles");
+}
+$datas = $requete->fetchAll(PDO::FETCH_ASSOC);
+var_dump($datas);
+echo $datas[0]["Titre"];
 ?>
 <h1>Bienvenue sur notre BLOG</h1>
-<ul>
-    <li><a href="eleve.php?nom=Enzo&note=A">Enzo</a></li>
-    <li><a href="eleve.php?nom=Bastien&note=C">Bastien</a></li>
-    <li><a href="eleve.php?nom=Evan&note=A">Evan</a></li>
-    <li><a href="eleve.php?nom=Raphael&note=B">Raphael</a></li>
-</ul>
-<form method="post" action="submitform.php">
+
+<form method="post">
     <input type="text" placeholder="Rechercher ..." name="search">
     <input type="hidden" value="1234" name="id">
 </form>
-
-    <form method="post">
-        <input type="text" name="prenom">
-        <input type="text" name="nom">
-        <input type="date" name="naissance">
-        <input type="submit">
-    </form>
 
 
 <?php   require("./inc/footer.php"); ?>
