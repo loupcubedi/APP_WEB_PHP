b57ae2ac11fad09155d0b1444e5d9d3fd12c16b1
loup <loup@gmail.com>
2023-12-14 16:04:02 +0100

    joijho ee d

---
+++ src/Controller/AdminArticleController.php
@@ -0,0 +1,8 @@
+namespace src\Controller;
+
+use src\Model\Article;
+
+class AdminArticleController extends AbstractController
+{
+    public function list()
+    {
---
+++ var/cache/8e/8e16a0b0cc998f01714e9111c4ced49f.php
@@ -0,0 +1,62 @@
+<?php
+
+use Twig\Environment;
+use Twig\Error\LoaderError;
+use Twig\Error\RuntimeError;
+use Twig\Extension\SandboxExtension;
+use Twig\Markup;
+use Twig\Sandbox\SecurityError;
+use Twig\Sandbox\SecurityNotAllowedTagError;
+use Twig\Sandbox\SecurityNotAllowedFilterError;
+use Twig\Sandbox\SecurityNotAllowedFunctionError;
+use Twig\Source;
+use Twig\Template;
+
+/* Article/index.html.twig */
+class __TwigTemplate_aab77d265086abd0f440a509a63708a8 extends Template
+{
+    private $source;
+    private $macros = [];
+
+    public function __construct(Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->source = $this->getSourceContext();
+
+        $this->parent = false;
+
+        $this->blocks = [
+        ];
+    }
+
+    protected function doDisplay(array $context, array $blocks = [])
+    {
+        $macros = $this->macros;
+        // line 1
+        echo "<h1>Voici les 20 derniers articles</h1>
+";
+    }
+
+    /**
+     * @codeCoverageIgnore
+     */
+    public function getTemplateName()
+    {
+        return "Article/index.html.twig";
+    }
+
+    /**
+     * @codeCoverageIgnore
+     */
+    public function getDebugInfo()
+    {
+        return array (  37 => 1,);
+    }
+
+    public function getSourceContext()
+    {
+        return new Source("<h1>Voici les 20 derniers articles</h1>
+", "Article/index.html.twig", "/var/www/html/src/View/Article/index.html.twig");
+    }
+}
