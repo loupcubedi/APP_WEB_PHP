fd8a93209179f1ab141dd46eb5a72c0a44a1ab10
fabien <flierville@cesi.fr>
2023-12-14 14:54:35 +0100

    Router

--- public/index.php
+++ public/index.php
@@ -12,5 +12,31 @@ function chargerClasse($class)
     }
 }
 spl_autoload_register("chargerClasse");
+//Routeur
+$controller = (isset($_GET["controller"])) ? $_GET["controller"] : '';
+$action = (isset($_GET["action"])) ? $_GET["action"] : '';
+$param = (isset($_GET["param"])) ? $_GET["param"] : '';
 
+if($controller != ''){
+    try{
+        $class = "src\Controller\\".$controller."Controller";
+        //src\Controller\ArticleController
+        if(class_exists($class)){
+            $controller = new $class;
+            if(method_exists($class,$action)){
+                echo $controller->$action($param);
+            }else{
+                throw new Exception("Méthode $action inexistante dans $class");
+            }
+        }else{
+            throw new Exception("Controller $class inexistant");
+        }
+    }catch (Exception $e){
+        // On verra plus tard pour faire un ErrorController
+        echo $e->getMessage();
+    }
+}else{
+    $controller = new \src\Controller\ArticleController();
+    echo $controller->index();
+}
 
